0bd0243d977cce153211568cb5a63a96a4840f2b
landolavio <landonlaviolette@gmail.com>
2024-04-11 21:02:12 -0500

    display incorrect password message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,24 +3,32 @@ import { AuthService } from '../shared/auth.service';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ShiftService } from '../shared/shift.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   employeeId: number=0;
   password: string = '';
+  errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router, private shiftService: ShiftService){}
 
   onSubmit(): void {
+    if (!this.employeeId || !this.password) {
+      this.errorMessage = "Employee ID and password are required.";
+      return;
+    }
+
     this.authService.login(this.employeeId, this.password)
       .subscribe({
         next: () => {
+          this.errorMessage = ''; // Clear any existing error message.
           const userRole = this.authService.getUserRole();
           this.shiftService.startShift();
           if (userRole === 'sales') {
@@ -32,8 +40,16 @@ export class LoginComponent {
           }
         },
         error: (error) => {
-          console.error('Login Failed: ', error);
+          // Set error message based on the response from the server
+          const errorMessage = error.error.message || 'Incorrect Password';
+          if (errorMessage.toLowerCase().includes('password')) {
+            this.errorMessage = 'Error: Incorrect password';
+          } else if (errorMessage.toLowerCase().includes('not found')) {
+            this.errorMessage = 'Error: User not found';
+          } else {
+            this.errorMessage = 'Login Failed: ' + errorMessage;
+          }
         }
       });
-    }
+  }
 }
