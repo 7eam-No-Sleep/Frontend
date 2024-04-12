a43201866fbd6377a6156d1c1b77daf7b94d8a71
landolavio <landonlaviolette@gmail.com>
2024-04-11 22:55:06 -0500

    all filters done

--- src/app/gift-cards/view-gc/view-gc.component.ts
+++ src/app/gift-cards/view-gc/view-gc.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { CardsService } from '../../shared/cards.service';
 import { GiftCard } from '../giftcard.model';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-gc',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './view-gc.component.html',
   styleUrl: './view-gc.component.css'
 })
 export class ViewGcComponent {
   cards: GiftCard[] = [];
+  filteredCards: GiftCard [] = [];
+  searchQuery: string = '';
 
   constructor(private cardsService: CardsService){}
 
@@ -21,6 +24,16 @@ export class ViewGcComponent {
   getCards(): void{
     this.cardsService.getCards()
     .subscribe(cards => this.cards = cards);
+  }
+  searchInventory(){
+    if(!this.searchQuery || this.searchQuery==''){
+      this.filteredCards = [...this.cards];
+    }else{
+      const query = parseInt(this.searchQuery);
+      this.filteredCards = this.cards.filter((card)=>
+        card.CardNumber === query
+      );
+    }
   }
 
 }
--- src/app/view-shifts/view-shifts.component.ts
+++ src/app/view-shifts/view-shifts.component.ts
@@ -3,17 +3,20 @@ import { ShiftService } from '../shared/shift.service';
 import { Shift } from './shift.model';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-view-shifts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './view-shifts.component.html',
   styleUrl: './view-shifts.component.css'
 })
 export class ViewShiftsComponent {
   shifts: Shift[] = [];
+  searchQuery: string='';
+  filteredShifts: Shift[] = [];
 
   
   constructor(private shiftService: ShiftService, private router: Router){}
@@ -31,6 +34,16 @@ export class ViewShiftsComponent {
     this.router.navigate(['/shift', shiftId, 'edit'])
   }
 
+  searchShifts(){
+    if(!this.searchQuery || this.searchQuery==''){
+      this.filteredShifts = [...this.shifts];
+  } else{
+    const query = parseInt(this.searchQuery);
+    this.filteredShifts = this.shifts.filter((shift) =>
+      shift.employee_id === query ||
+      shift.shift_date === this.searchQuery
 
-
+  );
+  }
+}
 }
